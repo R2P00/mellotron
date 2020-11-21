e271cf9a6a4429036b62fb40e63e4ea8f1341ccf
smilemcm <smilemcm@gmail.com>
2020-11-21 17:38:13 +0900

    modify hparam

--- hparams.py
+++ hparams.py
@@ -24,9 +24,11 @@ def create_hparams(hparams_string=None, verbose=False):
         ################################
         # Data Parameters             #
         ################################
-        training_files='filelists/ljs_audiopaths_text_sid_train_filelist.txt',
-        validation_files='filelists/ljs_audiopaths_text_sid_val_filelist.txt',
+        training_files='filelists/libritts_train_clean_100_audiopath_text_sid_shorterthan10s_atleast5min_train_filelist.txt',
+        validation_files='filelists/libritts_train_clean_100_audiopath_text_sid_atleast5min_val_filelist.txt',
         text_cleaners=['english_cleaners'],
+
+
         p_arpabet=1.0,
         cmudict_path="data/cmu_dictionary",
 
